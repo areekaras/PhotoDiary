50493b5dbea73685fc5839138da59ac7e4800caf
areekaras <shibili.areekara@gmail.com>
2022-03-27 20:37:49 +0530

    Render prototype feed data on the tableview

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -13,6 +13,51 @@ class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) var locationLabel: UILabel!
     @IBOutlet private(set) var tempLabel: UILabel!
     @IBOutlet private(set) var feedImageView: UIImageView!
+    
+    @IBOutlet private(set) var dateBGView: UIView!
+    @IBOutlet private(set) var locationTempBGView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        dateBGView.setDiagonalBackground()
+        locationTempBGView.setHorizontalBackground()
+    }
+}
+
+
+private extension UIView {
+
+    func setDiagonalBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        let black = UIColor.black
+        gradientLayer.colors = [
+            black.cgColor,
+            UIColor.clear.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.5)
+        gradientLayer.locations = [0.0, 1.0]
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func setHorizontalBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        let black = UIColor.black
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            black.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.2, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.2, y: 1.0)
+        gradientLayer.locations = [0.0, 1.0]
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
 }
 
 
--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -17,19 +17,29 @@ struct FeedImageViewModel {
 
 class FeedViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    // MARK: - Table view data source
+    private let feed = FeedImageViewModel.prototypeFeed
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return feed.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.row]
+        cell.configure(with: model)
+        return cell
     }
     
+}
+
+
+extension FeedImageCell {
+    func configure(with model: FeedImageViewModel) {
+        self.monthLabel.text = model.month
+        self.dayLabel.text = model.day
+        self.locationLabel.text = model.location
+        self.tempLabel.text = model.temp
+        self.feedImageView.image = UIImage(named: model.imageName)
+    }
 }
